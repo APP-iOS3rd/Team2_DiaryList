f0e65962526502362108e8dd8dda7e448187fce9
NohJooYoung <nohjoo97@naver.com>
2023-11-23 13:47:40 +0900

    [Add] 초기 설정

---
+++ Team2_DiaryList/Team2_DiaryList/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Team2_DiaryList
+//
+//  Created by 노주영 on 2023/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
